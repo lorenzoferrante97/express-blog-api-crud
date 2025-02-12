a2778c53407a6955a7236cd24d5951a2ca525d7a
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-12 14:40:12 +0100

    add: import post controller + route index + export router

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,4 +1,11 @@
 
+import express from 'express';
+const router = express.Router();
 
 // import post controller
 import postController from '../controllers/postController.js';
+
+// route -> index
+router.get('/', postController.index);
+
+export default router;
