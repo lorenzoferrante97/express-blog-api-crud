84183cf4e8f602260bc9bcf6ac946f547306eb10
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-12 15:19:37 +0100

    add: routes

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,32 +5,32 @@ import arrayPosts from '../data/posts.js';
 
 // function -> index
 const index = (req, res) => {
-
+    res.json(arrayPosts);
 };
 
 // function -> show
 const show = (req, res) => {
-
+    res.send("show funziona");
 };
 
 // function -> store
 const store = (req, res) => {
-
+    res.send("store funziona");
 };
 
 // function -> update
 const update = (req, res) => {
-
+    res.send("update funziona");
 };
 
 // function -> modify
 const modify = (req, res) => {
-
+    res.send("modify funziona");
 };
 
 // function -> destroy
 const destroy = (req, res) => {
-
+    res.send("destroy funziona");
 };
 
 export default {index, show, store, update, modify, destroy};
--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -8,4 +8,19 @@ import postController from '../controllers/postController.js';
 // route -> index
 router.get('/', postController.index);
 
+// route -> show
+router.get('/:id', postController.show);
+
+// route -> store
+router.post('/', postController.store);
+
+// route -> update
+router.put('/:id', postController.update);
+
+// route -> modify
+router.patch('/:id', postController.modify);
+
+// route -> destroy
+router.delete('/:id', postController.destroy);
+
 export default router;
