b401195eb7fef1466a8acf30e66df69507c1697b
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-14 11:02:03 +0100

    add: error 404 code

--- middlewares/errors/notFound.js
+++ middlewares/errors/notFound.js
@@ -1,3 +1,11 @@
 const errorNotFound = (err, req, res) => {
+
+    const notFound = {
+        status: 404,
+        error: "Risorsa non trovata",
+        message: "La risorsa a cui vuoi accedere non è stata trovata"
+    }
+
+    res.status(404).json(notFound);
     
 }
