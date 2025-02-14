d22e3489bd1adfeccedeb7ca00abbce084f17cb2
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-02-14 11:09:35 +0100

    fix: middleware

--- middlewares/errors/notFound.js
+++ middlewares/errors/notFound.js
@@ -1,12 +1,12 @@
-const errorNotFound = (err, req, res) => {
+const errorNotFound = (req, res) => {
 
     const notFound = {
-        status: 404,
         error: "Risorsa non trovata",
         message: "La risorsa a cui vuoi accedere non è stata trovata"
     }
 
-    res.status(404).json(notFound);
+    res.status(404);
+    res.json(notFound);
     
 }
 
