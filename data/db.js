8244c5c3885972d499e18610cc90904414352b57
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-13 12:51:28 +0100

    edit: es6 syntax

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 // database blog
 
-const mysql = require('mysql2');
+import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -8,3 +8,11 @@ const connection = mysql.createConnection({
   password: 'rootroot',
   database: 'blog_db',
 });
+
+connection.connect((err) => {
+  if (err) throw err;
+
+  console.log('Connected to MySQL!');
+});
+
+export default connection;
