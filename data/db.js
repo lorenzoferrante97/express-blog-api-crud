587870ae55731dad3a0b71bc1466cdb4105b59b5
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-13 12:41:58 +0100

    add: db connection

--- data/db.js
+++ data/db.js
@@ -1,1 +1,10 @@
 // database blog
+
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'rootroot',
+  database: 'blog_db',
+});
